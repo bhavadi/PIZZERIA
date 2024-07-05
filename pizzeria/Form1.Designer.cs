fb46e7a75bb1e96ace7ffc907bb292848f445e6c
bhavadi <bhavadi2005@gmail.com>
2024-07-05 11:19:24 +0200

    Initial commit

---
+++ pizzeria/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace PizzeriaKG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBezar = new System.Windows.Forms.Button();
+            this.btnTorol = new System.Windows.Forms.Button();
+            this.btnSzamol = new System.Windows.Forms.Button();
+            this.btnadatBe = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(241, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 33);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "MENÜVÁLASZTÉK";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Brown;
+            this.label2.Location = new System.Drawing.Point(203, 354);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Fizetendő:";
+            // 
+            // btnBezar
+            // 
+            this.btnBezar.Location = new System.Drawing.Point(640, 406);
+            this.btnBezar.Name = "btnBezar";
+            this.btnBezar.Size = new System.Drawing.Size(99, 23);
+            this.btnBezar.TabIndex = 11;
+            this.btnBezar.Text = "Bezár";
+            this.btnBezar.UseVisualStyleBackColor = true;
+            // 
+            // btnTorol
+            // 
+            this.btnTorol.Location = new System.Drawing.Point(62, 406);
+            this.btnTorol.Name = "btnTorol";
+            this.btnTorol.Size = new System.Drawing.Size(85, 23);
+            this.btnTorol.TabIndex = 10;
+            this.btnTorol.Text = "Töröl";
+            this.btnTorol.UseVisualStyleBackColor = true;
+            // 
+            // btnSzamol
+            // 
+            this.btnSzamol.Location = new System.Drawing.Point(62, 356);
+            this.btnSzamol.Name = "btnSzamol";
+            this.btnSzamol.Size = new System.Drawing.Size(85, 23);
+            this.btnSzamol.TabIndex = 9;
+            this.btnSzamol.Text = "Számol";
+            this.btnSzamol.UseVisualStyleBackColor = true;
+            // 
+            // btnadatBe
+            // 
+            this.btnadatBe.Location = new System.Drawing.Point(287, 406);
+            this.btnadatBe.Name = "btnadatBe";
+            this.btnadatBe.Size = new System.Drawing.Size(183, 23);
+            this.btnadatBe.TabIndex = 8;
+            this.btnadatBe.Text = "Adatbevitel";
+            this.btnadatBe.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(93, 118);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(589, 192);
+            this.panel1.TabIndex = 13;
+            // 
+            // Pizzaform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.BackgroundImage = global::pizzeria.Properties.Resources.a;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBezar);
+            this.Controls.Add(this.btnTorol);
+            this.Controls.Add(this.btnSzamol);
+            this.Controls.Add(this.btnadatBe);
+            this.Name = "Pizzaform";
+            this.Text = "Pizzéria";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBezar;
+        private System.Windows.Forms.Button btnTorol;
+        private System.Windows.Forms.Button btnSzamol;
+        private System.Windows.Forms.Button btnadatBe;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ pizzeria/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzeriaKG
+{
+    public partial class Form1 : Form
+    {
+        private bool lathato;
+
+        private List<Pizza> pizzak = new List<Pizza>();
+        private List<CheckBox> jeloloNegyzet = new List<CheckBox>();
+        private List<RadioButton> rdBtnKicsiArak = new List<RadioButton>();
+        private List<RadioButton> rdBtnNagyArak = new List<RadioButton>();
+        private List<TextBox> txtDarabok = new List<TextBox>();
+
+        private Panel pnlkozponti;
+        private TextBox txtFizetendo;
+
+        private int bal = 20;
+        private int fent = 10;
+        private int kozy = 40;
+        private int meretY = 20;
+        private int panelx = 200;
+        private int meretChk = 120;
+        private int meretAr = 50;
+        private int meretFt = 40;
+        private int meretDb = 50;
+        private int koz = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog1.FileName = "";
+            lathato = false;
+            LathatosagBeallitas(lathato);
+
+            pnlkozponti = new Panel();
+            pnlkozponti.Size = new Size(800, 600);
+            pnlkozponti.Location = new Point(10, 10);
+            this.Controls.Add(pnlkozponti);
+
+            txtFizetendo = new TextBox();
+            txtFizetendo.Location = new Point(10, 620);
+            txtFizetendo.Size = new Size(200, 30);
+            this.Controls.Add(txtFizetendo);
+
+            Button btnSzamol = new Button();
+            btnSzamol.Text = "Számol";
+            btnSzamol.Location = new Point(220, 620);
+            btnSzamol.Click += new EventHandler(btnSzamol_Click);
+            this.Controls.Add(btnSzamol);
+        }
+
+        public class Pizza
+        {
+            public string Nev { get; private set; }
+            public int ArKicsi { get; set; }
+            public int ArNagy { get; set; }
+
+            public Pizza(string nev, int arKicsi, int arNagy)
+            {
+                Nev = nev;
+                ArKicsi = arKicsi;
+                ArNagy = arNagy;
+            }
+
+            public override string ToString()
+            {
+                return this.Nev;
+            }
+        }
+
+        private void LathatosagBeallitas(bool lathatoE)
+        {
+            foreach (Control item in this.Controls)
+            {
+                item.Visible = lathatoE;
+            }
+            btnadatBe.Visible = !lathatoE;
+        }
+
+        private void btnadatBe_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fajlNev = openFileDialog1.FileName;
+
+                using (StreamReader olvasoCsatorna = new StreamReader(fajlNev))
+                {
+                    try
+                    {
+                        AdatBeolvasas(olvasoCsatorna);
+                        ValasztekFeltoltes();
+                        lathato = true;
+                        LathatosagBeallitas(lathato);
+                        btnadatBe.Visible = false;
+                        this.BackgroundImage = null;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Hibaüzenet a fejlesztő számára");
+                    }
+                    finally
+                    {
+                        if (olvasoCsatorna != null)
+                        {
+                            olvasoCsatorna.Close();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ValasztekFeltoltes()
+        {
+            CheckBox checkBox;
+            Label label;
+            RadioButton radioButton;
+            Panel panel;
+            TextBox textBox;
+
+            for (int i = 0; i < pizzak.Count; i++)
+            {
+                checkBox = new CheckBox();
+                checkBox.TextAlign = ContentAlignment.MiddleLeft;
+                checkBox.Text = pizzak[i].Nev;
+                checkBox.Location = new Point(bal, fent + i * kozy);
+                checkBox.Size = new Size(meretChk, meretY);
+                jeloloNegyzet.Add(checkBox);
+                pnlkozponti.Controls.Add(checkBox);
+
+                panel = new Panel();
+                panel.Size = new Size(panelx, meretY);
+                panel.Location = new Point(bal + meretChk, fent + i * kozy);
+                pnlkozponti.Controls.Add(panel);
+
+                radioButton = new RadioButton();
+                radioButton.Size = new Size(meretAr, meretY);
+                radioButton.TextAlign = ContentAlignment.MiddleRight;
+                radioButton.Text = pizzak[i].ArKicsi.ToString();
+                radioButton.Location = new Point(0, 0);
+                rdBtnKicsiArak.Add(radioButton);
+                panel.Controls.Add(radioButton);
+
+                radioButton = new RadioButton();
+                radioButton.Size = new Size(meretAr, meretY);
+                radioButton.TextAlign = ContentAlignment.MiddleRight;
+                radioButton.Text = pizzak[i].ArNagy.ToString();
+                radioButton.Location = new Point(meretAr + koz, 0);
+                rdBtnNagyArak.Add(radioButton);
+                panel.Controls.Add(radioButton);
+
+                label = new Label();
+                label.TextAlign = ContentAlignment.MiddleLeft;
+                label.Text = " Ft";
+                label.Location = new Point(2 * meretAr + koz, 0);
+                label.Size = new Size(meretFt, meretY);
+                panel.Controls.Add(label);
+
+                textBox = new TextBox();
+                textBox.Location = new Point(2 * meretAr + koz + meretFt + koz, 0);
+                textBox.Size = new Size(meretDb, meretY);
+                txtDarabok.Add(textBox);
+                panel.Controls.Add(textBox);
+
+                label = new Label();
+                label.TextAlign = ContentAlignment.MiddleLeft;
+                label.Text = " db";
+                label.Location = new Point(2 * meretAr + koz + meretFt + koz + meretDb + koz, 0);
+                label.Size = new Size(meretFt, meretY);
+                panel.Controls.Add(label);
+            }
+        }
+
+        private void btnSzamol_Click(object sender, EventArgs e)
+        {
+            bool vankijeloles = false;
+            int db, osszeg = 0, ar = 0;
+
+            for (int i = 0; i < jeloloNegyzet.Count; i++)
+            {
+                if (jeloloNegyzet[i].Checked)
+                {
+                    vankijeloles = true;
+                    try
+                    {
+                        if (rdBtnKicsiArak[i].Checked)
+                            ar = pizzak[i].ArKicsi;
+                        else if (rdBtnNagyArak[i].Checked)
+                            ar = pizzak[i].ArNagy;
+                        else
+                            throw new MissingFieldException();
+
+                        db = int.Parse(txtDarabok[i].Text);
+
+                        if (db <= 0) throw new Exception();
+
+                        txtDarabok[i].BackColor = Color.White;
+                        osszeg += ar * db;
+                    }
+                    catch (MissingFieldException)
+                    {
+                        MessageBox.Show("Nem választott méretet", "Hiba");
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Hibásan adta meg a darabszámot", "Hiba");
+                        txtDarabok[i].BackColor = Color.Coral;
+                        txtDarabok[i].Clear();
+                    }
+                }
+            }
+
+            if (!vankijeloles)
+            {
+                MessageBox.Show("Nincs kijelölve semmi", "Figyelmeztetés");
+            }
+            else
+            {
+                txtFizetendo.Text = osszeg + " Ft";
+            }
+        }
+
+        private void AdatBeolvasas(StreamReader olvasoCsatorna)
+        {
+            pizzak.Clear();
+
+            string sor;
+            while ((sor = olvasoCsatorna.ReadLine()) != null)
+            {
+                string[] adatok = sor.Split(';');
+                if (adatok.Length >= 3)
+                {
+                    string nev = adatok[0];
+                    int arKicsi, arNagy;
+                    if (int.TryParse(adatok[1], out arKicsi) && int.TryParse(adatok[2], out arNagy))
+                    {
+                        Pizza pizza = new Pizza(nev, arKicsi, arNagy);
+                        pizzak.Add(pizza);
+                    }
+                }
+            }
+        }
+
+        private void btnBezar_Click(object sender, EventArgs e)
+        {
+            DialogResult valasz = MessageBox.Show("Biztosan kilép?", "Megerősítés", MessageBoxButtons.YesNo);
+            if (valasz == DialogResult.Yes) this.Close();
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            txtFizetendo.Text = "";
+
+            foreach (Control item in pnlkozponti.Controls)
+            {
+                if (item is CheckBox)
+                {
+                    ((CheckBox)item).Checked = false;
+                }
+                else if (item is TextBox)
+                {
+                    ((TextBox)item).Clear();
+                }
+                else if (item is Panel || item is GroupBox)
+                {
+                    foreach (Control child in item.Controls)
+                    {
+                        if (child is RadioButton)
+                        {
+                            ((RadioButton)child).Checked = false;
+                        }
+                        else if (child is TextBox)
+                        {
+                            ((TextBox)child).Clear();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
